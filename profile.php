44087fdd00d56ae53b5de237f05095b013b99595
System Error <LOGAN2413@yandex.ru>
2025-02-12 17:21:13 +0300

    begining dev of the 'friend' page

--- profile.php
+++ profile.php
@@ -36,12 +36,14 @@ function get_list_message_user(){
         foreach ($result_friend as $item){
             echo('
                 <a class="friend-people user-item" data-user-id="'.$item['id'].'" data-user-name="' . $item['name'] . ' ' . $item['surname'] . '">
-                    <div class="friend-photo-people">
-                        <img src="assets/img/zamer.png" alt="Фото друга">
-                    </div>
-                    <div class="friend-info-people">
-                        <div class="friend-name-people" id="name">' . $item['name'] . ' ' . $item['surname'] . '</div>
-                        <div class="friend-message-people">?</div>
+                    <div class="friend-info-block-txt">
+                        <div class="friend-photo-people">
+                            <img src="assets/img/zamer.png" alt="Фото друга">
+                        </div>
+                        <div class="friend-info-people">
+                            <div class="friend-name-people" id="name">' . $item['name'] . ' ' . $item['surname'] . '</div>
+                            <div class="friend-message-people">?</div>
+                        </div>
                     </div>
                     <div class="friend-notification">
                         <div class="notification-count">???</div>
@@ -69,13 +71,22 @@ function get_users(){
     if (!$result_friend == ''){
         foreach ($result_friend as $item){
             echo('
-                <div class="friend" >
-                    <div class="friend-photo">
-                        <img src="assets/img/zamer.png" alt="Фото друга">
-                    </div>
-                    <div class="friend-info">
-                        <div class="friend-name">'. $item['name']. ' '. $item['surname'].'</div>
-                        <a href="" class="contact-friend">Написать сообщение</a>
+                <div class="friend">
+                    <div class="friend-block-photo-and-info">
+                        <div class="friend-photo">
+                            <img src="assets/img/zamer.png" alt="Фото друга">
+                        </div>
+                        <div class="friend-info">
+                            <div class="friend-name">'. $item['name']. ' '. $item['surname'].'</div>
+                            <a href="" class="friend_chat_to_link">
+                                <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M21 2H5C3.897 2 3 2.894 3 3.992V16.008C3 17.106 3.897 18 5 18V21L11 18H21C22.103 18 23 17.106 23 16.008V3.992C22.9984 3.46279 22.7869 2.95583 22.412 2.58237C22.037 2.20891 21.5292 1.99947 21 2Z" fill="#0B5ED7"/>
+                                    <path d="M9.5 10C9.5 10.8284 8.82843 11.5 8 11.5C7.17157 11.5 6.5 10.8284 6.5 10C6.5 9.17157 7.17157 8.5 8 8.5C8.82843 8.5 9.5 9.17157 9.5 10Z" fill="#D9D9D9"/>
+                                    <path d="M14.5 10C14.5 10.8285 13.8284 11.5 13 11.5C12.1716 11.5 11.5 10.8285 11.5 10C11.5 9.1716 12.1716 8.50003 13 8.50003C13.8284 8.50003 14.5 9.1716 14.5 10Z" fill="#D9D9D9"/>
+                                    <path d="M19.5 10C19.5 10.8284 18.8284 11.5 18 11.5C17.1716 11.5 16.5 10.8284 16.5 10C16.5 9.17157 17.1716 8.5 18 8.5C18.8284 8.5 19.5 9.17157 19.5 10Z" fill="#D9D9D9"/>
+                                </svg>
+                            </a>
+                        </div>
                     </div>
                     <div class="delete_friend">
                         <div class="delete_friend_icon"><img src="assets/img/icon_delete.png" alt=""></div>
