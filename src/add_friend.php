9f4dd801fd0c0230aba0b93cc6250ff28bff7826
live-sistem <LOGAN2413@yandex.ru>
2025-02-26 10:10:01 +0300

    A small change

--- src/add_friend.php
+++ src/add_friend.php
@@ -36,53 +36,25 @@ if (isset($data['User_id'])) {
 
     $result_messages = mysqli_query($connect, $sql);
 
-    echo("' Это id друга ' $friendId, 
-            ' Логин пользователя, которого добавляем в друзья ' $friendLogin,
-            ' Имя пользователя, которого добавляем в друзья ' $friendName,
-            ' Это id нынешнего пользователя ' $userId, 
-            ' Это ответ из Базы данных ',"); 
+    echo("
+        ' Это id друга ' $friendId, 
+        ' Логин пользователя, которого добавляем в друзья ' $friendLogin,
+        ' Имя пользователя, которого добавляем в друзья ' $friendName,
+        ' Это id нынешнего пользователя ' $userId, 
+        ' Это ответ из Базы данных ',"); 
 
     print_r($result_messages);
 
     if ($result_messages -> num_rows > 0){
         while ($row = mysqli_fetch_assoc($result_messages)) {
             echo "Найдена запись: " . print_r($row, true);
-            
         }
-    }else{
+    }
+    else{
         echo('Вот умора, она работает.');
 
         $result_messages = mysqli_query($connect, $sql_insert);
     }
-   
-    
-
-    // if ($result_messages) {
-    //     if ($result_messages -> num_rows > 0){
-    //         echo 'запрос был уже выполнен'; 
-    //         while ($row = mysqli_fetch_assoc($result_messages)) {
-    //             echo "Найдена запись: " . print_r($row, true);
-    //         }
-    //     }
-    //     else{
-    //         $status_friend = 'pending';
-            
-    //         $result_messages = mysqli_query($connect, 
-    //         "INSERT INTO `friends` (user_id, friend_id, status) 
-    //         VALUES ('$userId', '$friendId', $status_friend)");
-
-    //         
-    //     }
-    // } else {
-    //     echo "Ошибка запроса :" . $mysqli->error;
-    //     }   
-    
-        
-    // echo json_encode([
-    //     'status' => 'success',
-    //     'message' => "User ID: $userId is adding Friend with login: $friendLogin",
-    //     'sql' => "$result_messages"
-    // ]);
     echo 'Тут конец add_friend.php';
 } else {
     echo json_encode([
